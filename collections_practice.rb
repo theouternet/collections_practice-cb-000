c1c8a9ec9f6e0087ac8ee7d49bce284213579c00
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-16 23:56:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,12 +21,8 @@ end
 
 def find_a(array)
   
-   i = 0
-  while i < array.length |string|
-    return array[i] if yield(string[0])
-    i = i + 1
-  end
-  
+array.select do |string|
+  string
 end
 
 def kesha_maker(array)
