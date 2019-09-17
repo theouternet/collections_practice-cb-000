5a9b19398dbbc573417221e57e3f54edca74c518
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-17 02:07:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def find_a(array)
+array.select {|string| "a" == string[0]}
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+array.collect.with_index do |string, index| 
+if index == 1
+  string
+else
+  string + "s"
+end
+end
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |string|
+    string[2] = "$"
+    kesha<<string
+end
+kesha
+end
